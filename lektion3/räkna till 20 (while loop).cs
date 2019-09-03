e20c38a800193ab72cdee66f23370da7fab156ef
GabrielRamde <54704377+GabrielRamde@users.noreply.github.com>
2019-09-03 22:25:50 +0200

    Create räkna till 20 (while loop).cs

---
+++ lektion3/räkna till 20 (while loop).cs
@@ -0,0 +1,14 @@
+using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        int i = 0;
+         while (i < 20)
+        {
+         i++;
+         Console.WriteLine(i);
+         }
+    }
+}
